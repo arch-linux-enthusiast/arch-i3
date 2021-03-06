8c376d8800f9538fd36df26926214274f0d031c0
--replace-all <--replace-all>
2021-03-06 11:08:28 +0000

    modified:   bashrc 	new file:   config.fish

--- config.fish
+++ config.fish
@@ -1,11 +1,6 @@
-#
-# ~/.bashrc
-#
-
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
-
-alias ls='ls --color=auto'
+set EDITOR vim
+set BROWSER firefox
+clipmenud &
 alias yt-d="youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a] -o '%(title)s.%(ext)s'"
 alias mirrorlist-update='sudo reflector --verbose --protocol https --latest 20 --sort rate --save /etc/pacman.d/mirrorlist'
 alias grep='grep --color=auto'
@@ -21,26 +16,4 @@ alias install="sudo pacman -S"
 alias remove="sudo pacman -Rns"
 alias git-push="git commit -a && git push"
 
-function _update_ps1() {
-    PS1=$(powerline-shell $?)
-}
 
-if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
-    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-fi
-
-#export PAGER="/usr/bin/most -s"
-man() {
-    LESS_TERMCAP_md=$'\e[01;31m' \
-    LESS_TERMCAP_me=$'\e[0m' \
-    LESS_TERMCAP_se=$'\e[0m' \
-    LESS_TERMCAP_so=$'\e[01;44;33m' \
-    LESS_TERMCAP_ue=$'\e[0m' \
-    LESS_TERMCAP_us=$'\e[01;32m' \
-    command man "$@"
-}
-
-
-export BROWSER=/bin/firefox
-clipmenud &
-fish
