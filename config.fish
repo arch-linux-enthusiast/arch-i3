5641b785b7c04113ea60b6dc703d8c8ecf7d5c9f
arch-linux-enthusiast <anonymous9124591@protonmail.com>
2021-04-28 15:20:15 +0530

    modified:   config.fish

--- config.fish
+++ config.fish
@@ -1,7 +1,11 @@
+set fish_greeting ""
+set -g fish_cursor_insert line blink
+set -g fish_cursor_default line blink
+
 set EDITOR vim
-#set BROWSER firefox
 export EDITOR=vim
-#clipmenud &
+
+
 alias yt-d="youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a] -o '%(title)s.%(ext)s'"
 alias mirrorlist-update='sudo reflector --verbose --protocol https --latest 20 --sort rate --save /etc/pacman.d/mirrorlist'
 alias grep='grep --color=auto'
@@ -22,3 +26,9 @@ alias p="poweroff"
 alias c="clear"
 alias d="notify-send (date) && exit"
 alias wifi="nmcli radio wifi"
+#alias ssh="kitty +kitten ssh"
+alias pt="ping -c 3 8.8.8.8"
+alias u="vim Documents/Arch\ Linux/Common\ Unicode\ Charactars.txt"
+alias f="vim ~/.config/fish/config.fish"
+alias i="vim ~/.config/i3/config"
+
